6921b676d32ccac9b8d2b427afeb069a88889240
LAURABOEGLIN1 <65244109+LAURABOEGLIN1@users.noreply.github.com>
2020-05-19 14:39:46 +0200

    Class AAcategories corrigé

--- src/RCMAP/Classification_AA.py
+++ src/RCMAP/Classification_AA.py
@@ -6,27 +6,26 @@ class AAcategories:
             'Negative' : set(["D","E"]) ,
             'Positive' : set(["R","K"]) ,
             'Aliphatic' : set(["I","V","L"]) ,
-            'Tiny' : set(["A", "S","Csh","G"]) ,
+            'Tiny' : set(["A", "S","C","G"]) ,
             'Aromatic' : set(["F", "Y", "W", "H"]),
             'Charged' : set(["D", "E", "K", "R","H"]),
-            'Small' : set(["A", "S","Csh","G","P","N","D","T","V","Css"]),
-            'Polar' : set(["D", "E", "K", "R","H","Q","N","S","Csh","T","Y","W"]),
-            'Hydrophobic' : set(["I","V","L","F", "Y", "W", "H","M","K","T","Cs","G","A","Csh"])
+            'Small' : set(["A", "S","C","G","P","N","D","T","V"]),
+            'Polar' : set(["D", "E", "K", "R","H","Q","N","S","C","T","Y","W"]),
+            'Hydrophobic' : set(["I","V","L","F", "Y", "W", "H","M","K","T","G","A","C"])
         }
 
     def find_category(self,ens):
         """This function returns the smallest category in which the set is included"""
         if len(ens) == 1 :
-            return(print("The class contains only one amino acid : ",list(ens)[0]))
+            return(print(ens))
         List = ['Negative', 'Positive', 'Aliphatic', 'Tiny', 'Aromatic', 'Charged', 'Small', 'Polar','Hydrophobic']
         for k in range(len(self.categories)):
             if ens.issubset(self.categories[List[k]]):
-                return(print("The set is contained in the category : ",List[k]))
-        return(print("The set is not contained in any of the categories"))
+                return(print(List[k]))
+        return(print(set(["I","V","L","F", "Y", "W", "H","M","K","T","G","A","C","P","S","N","D","E","Q","R"])))
 
 
 ens = {"P"}
 objet = AAcategories()
 objet.find_category(ens)
-
 
---
+++ src/RCMAP/lecture datas.py
@@ -0,0 +1,3 @@
+data_open =open()
+data=data_open.read()
+print(data)
