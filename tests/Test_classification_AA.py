dff802fee18ab8513df58bba8d35387757f2da7f
LAURABOEGLIN1 <65244109+LAURABOEGLIN1@users.noreply.github.com>
2020-05-20 11:38:18 +0200

    test1

--- tests/Test_classification_AA.py
+++ tests/Test_classification_AA.py
@@ -5,9 +5,5 @@ from RCMAP.Classification_AA import AAcategories
 
 class MyTestCase(unittest.TestCase):
 
-    def test_sort_categories():
-        assert AAcategories.sort_categories(self) == ['Negative', 'Positive', 'Aliphatic', 'Tiny', 'Aromatic', 'Charged', 'Small', 'Polar', 'Hydrophobic']
-
-
-objet = MyTestCase()
-print(objet.test_sort_categories())
+    def test_sort_categories(self):
+        assert AAcategories().sort_categories() == ['Negative', 'Positive', 'Aliphatic', 'Tiny', 'Aromatic', 'Charged', 'Small', 'Polar', 'Hydrophobic']
