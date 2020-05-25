3c8d23d791698578f459e1ddeebaa2ab57f1f379
LAURABOEGLIN1 <65244109+LAURABOEGLIN1@users.noreply.github.com>
2020-05-25 18:16:16 +0200

    alignment1

--- src/RCMAP/Alignment.py
+++ src/RCMAP/Alignment.py
@@ -1,21 +1,27 @@
 from Bio import AlignIO
-from Bio import SeqIO
+from Bio.Align import MultipleSeqAlignment
 
 from RCMAP.Classification_AA import AAcategories
 
-file = "ArsM_aln.faa"  #Definition du fichier
+file ="ArsM_aln.faa"
+seqs_to_evaluate = ["WP_045226361.1", "Q969Z2"]
+class Alignments :
 
-def read_alignments(file, seqs_to_evaluate=["Q968Z2","WP_045226361"]):
-    alignment = AlignIO.read(file, "fasta")
-    seqeval = (s.seq for s in alignment if s.id == "Q968Z2" or s.id == "WP_045226361")
-    return (seqeval)
+    def read_alignments(self,file, seqs_to_evaluate):
+        alignment = AlignIO.read(file, "fasta")
+        self.seqeval = MultipleSeqAlignment([s for s in alignment if s.id in seqs_to_evaluate])
+        self.seqrefs = MultipleSeqAlignment([s for s in alignment if s.id not in seqs_to_evaluate])
+        return self.seqrefs, self.seqeval
 
-#def get_cat_at_pos(seqref,pos):
-    #lire set(aa) à la position dans chaque séquence de seqref
-    #return (find_category(set(aa)))
+    def get_cat_at_pos(self, pos):
+        AA_at_pos = set()
+        for k in range(len(self.seqrefs)) :
+            AA_at_pos.add(self.seqrefs[k][pos])
+        return (AAcategories().find_category(AA_at_pos))
 
-#def get_cat_in_range(seqref,debut=None,fin=None):
-    #return [get_cat_at_pos(seqref,pos) for pos in range(debut,fin)]
+    #def get_cat_in_range(seqref,debut=None,fin=None):
+        #return [get_cat_at_pos(seqref,pos) for pos in range(debut,fin)]
 
-
-print(read_alignments(file))
+object = Alignments()
+print(object.read_alignments(file,seqs_to_evaluate))
+print(object.get_cat_at_pos(3))
--- tests/test_Alignment.py
+++ tests/test_Alignment.py
@@ -1,19 +1,19 @@
 import unittest
+from RCMAP.Alignment import Alignments
+from Bio.Align import MultipleSeqAlignment
 from Bio import AlignIO
-from RCMAP.Classification_AA import AAcategories
-from tests.read_alignment import Read_sequences
-
 
 class MyTestCase(unittest.TestCase):
 
     def test_read_alignments(self):
-        (seqref,seqeval) = RCMAP.Alignment().read_alignments("data/ArsM_aln.faa")
-        assert [s.id for s in seqref] == ["gb|ACN39191.1","ref|XP_005539091.1","gb|PXF49941.1","gb|OSX76803.1","emb|SMG66974.1","ref|XP_005706547.1"]
-        assert [s.id for s in seqeval] == ["Q968Z2","WP_045226361"]
+        (seqref, seqeval) = Alignments().read_alignments("data/ArsM_aln.faa", ["Q969Z2", "WP_045226361.1"])
+        assert [s.id for s in seqref] == ["gb|ACN39191.1", "ref|XP_005539091.1", "gb|PXF49941.1", "gb|OSX76803.1",
+                                          "emb|SMG66974.1", "ref|XP_005706547.1"]
+        assert [s.id for s in seqeval] == ["WP_045226361.1", "Q969Z2"]
 
-
-#    def test_create_set_AA(self):
-#        assert Read_sequences.create_set_AA()== [{'M'}, {'G', 'S', 'D', 'H', 'P'}, {'D', '-', 'K'}, {'-', 'T'}, {'-', 'P'}, {'-', 'S'}, {'-', 'T'}, {'-', 'T'}, {'-', 'A'}, {'-', 'E'}, {'-', 'R'}, {'-', 'S'}, {'F', '-', 'H'}, {'-', 'L'}, {'F', 'I', '-', 'L'}, {'Q', '-', 'G', 'E'}]
+    def test_get_cat_at_pos(self):
+        seqrefs = Alignments().read_alignments("data/ArsM_aln.faa", ["Q969Z2", "WP_045226361.1"])[0]
+        assert Alignments().get_cat_at_pos(3) == {'T'}
 
 #    def test_create_set_categories(self):
 #        self.List_AA = [{'M'},{"-","K","D"},{"-","D"}]
