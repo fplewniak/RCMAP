12ab29ad1b6eadaec339d1e4149835bd5af7b38b
LAURABOEGLIN1 <65244109+LAURABOEGLIN1@users.noreply.github.com>
2020-05-26 17:02:32 +0200

    alignment4

--- src/RCMAP/Alignment.py
+++ src/RCMAP/Alignment.py
@@ -13,18 +13,37 @@ class Alignments :
         self.seqeval = MultipleSeqAlignment([s for s in alignment if s.id in seqs_to_evaluate])
         self.seqrefs = MultipleSeqAlignment([s for s in alignment if s.id not in seqs_to_evaluate])
 
-    def read_alignments(self):
+    def get_alignments(self):
         return self.seqrefs, self.seqeval
 
     def get_cat_at_pos(self, pos):
+        """
+        :param pos: position of the amino acid
+        :return:
+        """
         AA_at_pos = set()
         for k in range(len(self.seqrefs)) :
             AA_at_pos.add(self.seqrefs[k][pos])
         return (AAcategories().find_category(AA_at_pos))
 
-    #def get_cat_in_range(seqref,debut=None,fin=None):
-        #return [get_cat_at_pos(seqref,pos) for pos in range(debut,fin)]
+    def get_cat_in_range(self,pos1=None,pos2=None):
+        """
+        :param pos1: start position
+        :param pos2: end position
+        :return: list of the categories of amino acid at every position between pos1 and pos2
+        """
+        if pos1 == None :
+            pos1=0
+        if pos2 == None :
+            pos2=len(self.seqrefs[0])
+        if pos1 > len(self.seqrefs[0]) or pos2 > len(self.seqrefs[0]):
+            return "Error"
+        cat_in_range = []
+        for pos in range(pos1, pos2+1):
+            cat_in_range.append(self.get_cat_at_pos(pos))
+        return cat_in_range
 
-object = Alignments("ArsM_aln.faa",["WP_045226361.1", "Q969Z2"])
-#print(object.read_alignments(file,seqs_to_evaluate))
-print(object.get_cat_at_pos(3))
+#object = Alignments("ArsM_aln.faa",["WP_045226361.1", "Q969Z2"])
+#print(object.get_alignments(file,seqs_to_evaluate))
+#print(object.get_cat_at_pos(2))
+#print(object.get_cat_in_range(None,None))
--- tests/test_Alignment.py
+++ tests/test_Alignment.py
@@ -5,16 +5,23 @@ from Bio import AlignIO
 
 class MyTestCase(unittest.TestCase):
 
-    def test_read_alignments(self):
-        (seqref, seqeval) = Alignments("ArsM_aln.faa",["WP_045226361.1", "Q969Z2"]).read_alignments()
+    def test_get_alignments(self):
+        (seqref, seqeval) = Alignments("ArsM_aln.faa",["WP_045226361.1", "Q969Z2"]).get_alignments()
         assert [s.id for s in seqref] == ["gb|ACN39191.1", "ref|XP_005539091.1", "gb|PXF49941.1", "gb|OSX76803.1",
                                           "emb|SMG66974.1", "ref|XP_005706547.1"]
         assert [s.id for s in seqeval] == ["WP_045226361.1", "Q969Z2"]
 
     def test_get_cat_at_pos(self):
-        seqrefs = Alignments("ArsM_aln.faa",["WP_045226361.1", "Q969Z2"]).read_alignments()[0]
         assert Alignments("ArsM_aln.faa",["WP_045226361.1", "Q969Z2"]).get_cat_at_pos(3) == {'T'}
 
-#    def test_create_set_categories(self):
-#        self.List_AA = [{'M'},{"-","K","D"},{"-","D"}]
-#        assert Read_sequences.create_set_categories() == [{'M'},{'E', 'K', 'H', 'R', 'D'},{"D"}]
+    def test_get_cat_in_range1(self):
+        assert Alignments("ArsM_aln_part.faa",["WP_045226361.1", "Q969Z2"]).get_cat_in_range(1,3) == [{"I","V","L","F","Y","W","H","M","K","T","G","A","C","P","S","N","D","E","Q","R"},{"D","E","K","R","H"},{"T"}]
+
+    def test_get_cat_in_range2(self):
+        assert Alignments("ArsM_aln_part.faa", ["WP_045226361.1", "Q969Z2"]).get_cat_in_range(None, 2) == [{"M"},{"I","V","L","F","Y","W","H","M","K","T","G","A","C","P","S","N","D","E","Q","R"},{"D","E","K","R","H"}]
+
+    def test_get_cat_in_range3(self):
+        assert Alignments("ArsM_aln_part.faa", ["WP_045226361.1", "Q969Z2"]).get_cat_in_range(4, None) == [{"P"},{"S"},{"T"},{"T"},{"A"}]
+
+    def test_get_cat_in_range4(self):
+        assert Alignments("ArsM_aln_part.faa", ["WP_045226361.1", "Q969Z2"]).get_cat_in_range(0, 10) == "Error"
