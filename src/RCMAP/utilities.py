3fbbcd825cd8680a0b7d7cca07826246720740af
LAURABOEGLIN1 <65244109+LAURABOEGLIN1@users.noreply.github.com>
2020-05-28 09:55:02 +0200

    alignment8

---
+++ src/RCMAP/utilities.py
@@ -0,0 +1,22 @@
+def get_positions_list(positions):
+    """
+    :param positions:  #like ['3:10', '8:25' ,'32', '45:']
+    :return: #like [[3,10], [8,25], [32], [45,None]]
+    """
+    positions_list = []
+    for k in range(len(positions)):
+        l = []
+        if ':' in positions[k]:
+            r = positions[k].rpartition(':')
+            if r[0] != '':
+                l.append(int(r[0]))
+            else:
+                l.append(None)
+            if r[2] != '':
+                l.append(int(r[2]))
+            else:
+                l.append(None)
+            positions_list.append(l)
+        else:
+            positions_list.append([int(positions[k])])
+    return positions_list
