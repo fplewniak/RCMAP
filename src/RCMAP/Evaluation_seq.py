28a4e45d4a4a41f874cf3a6b905f55c8c0a3948e
LAURABOEGLIN1 <65244109+LAURABOEGLIN1@users.noreply.github.com>
2020-06-03 12:11:00 +0200

    Evaluation_seq NEW VERSION1 ERROR

--- src/RCMAP/Evaluation_seq.py
+++ src/RCMAP/Evaluation_seq.py
@@ -1,5 +1,5 @@
 from RCMAP.Alignment import Alignments
-from RCMAP.Utilities import get_positions_list, find_key
+from RCMAP.Utilities import get_positions_list
 from RCMAP.Classification_AA import AAcategories
 
 file = "ArsM_aln.faa"
@@ -10,28 +10,11 @@ alignments = Alignments(file, sequences_to_evaluate)
 list_of_positions = get_positions_list(positions)
 list_of_categories = alignments.get_category_list(list_of_positions)
 
-for s in range(len(sequences_to_evaluate)):
-    list_of_aa_in_seqeval = alignments.get_aa_list(list_of_positions, sequences_to_evaluate[s])
-    print(sequences_to_evaluate[s])
-    for k in range(len(list_of_categories)):
-        pos = list_of_positions[k][0]
-        for i in range(len(list_of_categories[k])):
-            print(pos, " : ", list_of_aa_in_seqeval[k][i], " ; Compatibility : ",
-                  AAcategories().compatibility(list_of_aa_in_seqeval[k][i],
-                                               list_of_categories[k][i])
-                  , " ; ",find_key(list_of_categories[k][i]), alignments.list_of_aa_ref[pos-1])
-            pos += 1
-
-# Nouvelle version
 for s in sequences_to_evaluate:
     print(s)
-    for k in range(len(list_of_positions)):
-        category = alignments.list_of_categories[
-                   list_of_positions[k][0] - 1:list_of_positions[k][1]]
-        pos = 0
-        for i in range(list_of_positions[k][0], list_of_positions[k][1] + 1):
+    for start, end in list_of_positions:
+        for i in range(start, end+1):
             aa = alignments.get_aa_at_pos(i, s)
             print(i, " : ", aa, " ; Compatibility : ",
-                  AAcategories().compatibility(set(aa), category[pos]), " ; ",
-                  find_key(category[pos]), alignments.list_of_aa_ref[i-1])
-            pos += 1
+                  AAcategories().compatibility(set(aa), alignments.get_cat_at_pos(i)), " ; ",
+                  alignments.get_cat_set_at_pos(i), alignments.get_aa_observed_at_pos(i))
