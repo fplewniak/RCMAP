3201443051b82c1866f523c057a9c3ca70f11068
fplewniak <f.plewniak@unistra.fr>
2020-06-04 14:58:25 +0200

    setup.py addition

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='RCMAP',
+    version='1.0.0',
+    packages=['RCMAP'],
+    package_dir={'': 'src'},
+    url='https://github.com/fplewniak/RCMAP',
+    license='CeCILL FREE SOFTWARE LICENSE AGREEMENT Version 2.1 dated 2013-06-21',
+    description='Residue Conservation in Multiple Alignment of Proteins ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Development Status :: 4 - Beta'
+        ],
+    python_requires='>=3.6', install_requires=['biopython'],
+    entry_points={
+        "console_scripts": [
+            "evaluate_seq = RCMAP.Evaluation_seq:main"
+            ]
+        }
+    )
--- src/RCMAP/Evaluation_seq.py
+++ src/RCMAP/Evaluation_seq.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 from RCMAP.Alignment import Alignments
-from RCMAP.Utilities import get_positions_list
+from RCMAP.utilities import get_positions_list
 from RCMAP.Classification_AA import AAcategories
 
 
@@ -16,31 +16,33 @@ def get_params(argv):
     parser.add_argument('--seqeval', metavar='string', nargs='+', help='Sequences to evaluate',
                         required=True)
     parser.add_argument('--positions', nargs='+', help='List of positions to evaluate',
-                        required=True)
+                        default=[':'])
     a = parser.parse_args()
     return a
 
 
-a = get_params(sys.argv[1:])
+def main():
+    a = get_params(sys.argv[1:])
 
-# file = "ArsM_aln.faa"
-# sequences_to_evaluate = ["WP_045226361.1", "Q969Z2"]
-# positions = ['2:4', '40', '50:52']
+    # file = "ArsM_aln.faa"
+    # sequences_to_evaluate = ["WP_045226361.1", "Q969Z2"]
+    # positions = ['2:4', '40', '50:52']
 
-alignments = Alignments(a.file, a.seqeval)
-list_of_positions = get_positions_list(a.positions)
-list_of_categories = alignments.get_category_list(list_of_positions)
+    alignments = Alignments(a.file, a.seqeval)
+    list_of_positions = get_positions_list(a.positions)
+    list_of_categories = alignments.get_category_list(list_of_positions)
 
-for s in a.seqeval:
-    print(s)
-    for start, end in list_of_positions:
-        for i in range(start, end + 1):
-            print('{pos} : {aa} : {test} : {cat} {obs}'.format(pos=i,
-                                                               aa=alignments.get_aa_at_pos(i, s),
-                                                               test=AAcategories().compatibility(
-                                                                   set(alignments.get_aa_at_pos(i,
-                                                                                                s)),
-                                                                   alignments.get_cat_at_pos(i)),
-                                                               cat=alignments.get_cat_set_at_pos(i),
-                                                               obs=alignments.get_aa_observed_at_pos(
-                                                                   i)))
+    for s in a.seqeval:
+        print(s)
+        for start, end in list_of_positions:
+            for i in range(start, end + 1):
+                print('{pos} : {aa} : {test} : {cat} {obs}'
+                      .format(pos=i,
+                              aa=alignments.get_aa_at_pos(i, s),
+                              test=AAcategories().compatibility(set(alignments.get_aa_at_pos(i, s)),
+                                                                alignments.get_cat_at_pos(i)),
+                              cat=alignments.get_cat_set_at_pos(i),
+                              obs=alignments.get_aa_observed_at_pos(i)))
+
+
+main()
