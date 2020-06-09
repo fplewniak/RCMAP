e73ec54051891fd34b697afe6c34d5a4e51fe674
fplewniak <f.plewniak@unistra.fr>
2020-06-09 15:57:58 +0200

    Changed width of position column and information content of position is now displayed with 3 significant digits.

--- src/RCMAP/evaluation_seq.py
+++ src/RCMAP/evaluation_seq.py
@@ -37,14 +37,14 @@ def main():
         for start, end in list_of_positions:
             for i in range(1 if start is None else start,
                            len(alignments.seqeval[0]) if end is None else end + 1):
-                print('{name_seq} : {pos} : {aa} : {test} : {info} : {cat} {obs}'.format(
-                    name_seq=s.rjust(0), pos=str(i).rjust(2),
+                print('{name_seq} : {pos} : {aa} : {test} : {info:.2f} : {cat} {obs}'.format(
+                    name_seq=s.rjust(0), pos=str(i).rjust(4),
                     aa=alignments.get_aa_at_pos(i, s),
                     test=str(AAcategories().compatibility(set(alignments.get_aa_at_pos(i, s)),
                                                       alignments.get_cat_at_pos(i))).rjust(6),
                     cat=str(alignments.get_cat_set_at_pos(i)).center(10),
                     obs=alignments.get_aa_observed_at_pos(i),
-                    info=str(round(alignments.information_pos(i, a.method, a.gaps),2)).center(8)))
+                    info=round(alignments.information_pos(i, a.method, a.gaps),2)))
 
 
 if __name__ == '__main__':
